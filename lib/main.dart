a4800c22ea799c279726b8b5c3357020c554bb15
Bishoy <bishoyabdo236@gmail.com>
2023-11-29 16:19:55 +0200

    get Stripe SDK setup ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'Features/Checkout/Presentation/Views/my_cart_view.dart';
 
-void main() {
+void main() async {
+  Stripe.publishableKey = "pk_test_51OHk75EBbIoZFxHc8KeGMortRwJINUt9NVtsj49vWK4HT8RwOYroebeC3Vc0zm9khABaXOjxzv5cH6XQNL3ahWea006Dgb2onr";
   runApp(const CheckoutApp());
 }
+
 
 class CheckoutApp extends StatelessWidget {
   const CheckoutApp({super.key});
