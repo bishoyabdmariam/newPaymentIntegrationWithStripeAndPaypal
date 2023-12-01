0ba5954a676db5394aa67165fc4e73cace8aaecd
Bishoy <bishoyabdo236@gmail.com>
2023-12-01 17:47:12 +0200

    add post payment

--- lib/core/utils/stripeService.dart
+++ lib/core/utils/stripeService.dart
@@ -1,11 +1,20 @@
 import 'package:paymentapp/Features/checkout/data/models/paymentIntentInputModel.dart';
 import 'package:paymentapp/Features/checkout/data/models/paymentIntentModel/PaymentIntent.dart';
+import 'package:paymentapp/core/utils/apiKeys.dart';
 import 'package:paymentapp/core/utils/apiService.dart';
 
-class StripeService{
+class StripeService {
   final ApiService apiService = ApiService();
 
-  PaymentIntent createPaymentIntent (PaymentIntentInputModel paymentIntentInputModel){
-    apiService.post(body: PaymentIntentInputModel(amount: '1000', currency: "usd").toJson(), url: 'https://api.stripe.com/v1/payment_intents', token: );
+  Future<PaymentIntent> createPaymentIntent(
+      PaymentIntentInputModel paymentIntentInputModel) async {
+    var response = await apiService.post(
+      body: PaymentIntentInputModel(amount: '1000', currency: "usd").toJson(),
+      url: 'https://api.stripe.com/v1/payment_intents',
+      token: ApiKeys.secretKey,
+    );
+
+    PaymentIntent paymentIntent = PaymentIntent.fromJson(response.data);
+    return paymentIntent;
   }
 }
